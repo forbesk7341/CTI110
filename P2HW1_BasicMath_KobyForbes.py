dc5451b6efc4e190864977bfb00be0cac3d0b4ae
forbesk7341 <61748565+forbesk7341@users.noreply.github.com>
2020-03-03 10:58:40 -0500

    Add files via upload

---
+++ P2HW1_BasicMath_KobyForbes.py
@@ -0,0 +1,10 @@
+# Adding and multiplying two numbers
+  # 2/25/2020
+  # CTI-110 P2HW1 - Basic Math
+  # Koby Forbes
+  #
+num1 = float(input('Enter first number. '))
+num2 = float(input('Enter second number. '))
+Sum =print (num1 + num2)
+Product =print (num1 * num2)
+
